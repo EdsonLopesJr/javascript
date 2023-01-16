99c5a7486e5fd718fdb44a150c92560ff434473e
Edson Lopes Jr <edson.eldsjr@gmail.com>
2023-01-16 20:40:00 -0300

    Create ex04.js

---
+++ function/exercicio-function/ex04.js
@@ -0,0 +1,8 @@
+// 04) Crie uma função que irá receber dois valores, o dividendo e o divisor. A função deverá imprimir o resultado
+// e o resto da divisão destes dois valores.
+
+function divisao(dividendo, divisor) {    
+    console.log( 'resultado = ',dividendo / divisor, '\nresto da divisão = ', dividendo % divisor)  
+}
+
+divisao(7,3)
