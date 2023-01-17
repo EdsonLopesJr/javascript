493ddc7ae97efbf89bee8972ca61aa38889fbc79
Edson Lopes Jr <edson.eldsjr@gmail.com>
2023-01-16 21:54:06 -0300

    Create orientationObject.js

---
+++ object/orientationObject.js
@@ -0,0 +1,17 @@
+//Código não Executavel;
+
+// Procedural exemplo:
+
+procedimento(valor1, valor2, valor3)
+
+// Orientação a Objeto:
+objeto = {
+    valor1,
+    valor2,
+    valor3, 
+    procedimento() {
+        //...
+    }
+}
+
+objeto.procedimento(); // foco passa a ser o objeto em vez da função.:
