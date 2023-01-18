f350358eb1854ab30367355ed9c388863c86302e
Edson Lopes Jr <edson.eldsjr@gmail.com>
2023-01-17 21:29:05 -0300

    Update notacao.js

--- object/notacao.js
+++ object/notacao.js
@@ -8,3 +8,28 @@ const obj1 = {a: a, b: b, c: c}; // Antes
 const obj2 = {a, b, c}; // Depois
 
 console.log(obj1, obj2);
+console.log('---------------------------------------------');
+
+const nomeAttr = 'nota';
+const valorAttr = 7.89;
+
+const obj3 = {};
+obj3[nomeAttr] = valorAttr; // Antes
+console.log(obj3);
+
+const obj4 = {[nomeAttr]: valorAttr}; // Depois
+console.log(obj4);
+console.log('---------------------------------------------');
+
+//definição da função dentro do objeto tbm sofreu mudanças na nova versão:
+const obj5 = {
+    funcao1: function () {
+        // antes
+    }, 
+    funcao2() {
+        // depois
+    }
+};
+
+console.log(obj5);
+console.log('---------------------------------------------');
